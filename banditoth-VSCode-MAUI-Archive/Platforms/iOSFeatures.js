3d89f2163de8247d97205e2ec41132223638e748
Tim Stephansen <Tim.Stephansen@newmindtech.com>
2024-10-21 12:34:44 -0400

    Added support for the Xcode 16 provisioning profiles folder.

--- banditoth-VSCode-MAUI-Archive/Platforms/iOSFeatures.js
+++ banditoth-VSCode-MAUI-Archive/Platforms/iOSFeatures.js
@@ -43,7 +43,56 @@ function getSigningIdentities() {
 }
 
 function getProvisioningProfiles() {
+    const provisioningProfiles = [];
+    let pre16Profiles = getPreXcode16ProvisioningProfiles();
+    let currentProfiles = getXcode16ProvisioningProfiles();
+    for(let i = 0; i < pre16Profiles.length; i++) {
+        provisioningProfiles.push(pre16Profiles[i]);
+    }
+    for(let i = 0; i < currentProfiles.length; i++) {
+        provisioningProfiles.push(currentProfiles[i]);
+    }
+    return provisioningProfiles;
+}
+
+function getPreXcode16ProvisioningProfiles() {
     const provisioningProfilesPath = path.join(process.env.HOME, 'Library', 'MobileDevice', 'Provisioning Profiles');
+
+    try {
+        const files = fs.readdirSync(provisioningProfilesPath);
+
+        const profiles = files.map((file) => {
+            const filePath = path.join(provisioningProfilesPath, file);
+            try {
+                const content = fs.readFileSync(filePath, 'utf-8');
+                const expirationDateMatch = content.match(/<key>ExpirationDate<\/key>\s*<date>([^<]+)<\/date>/);
+                const nameMatch = content.match(/<key>Name<\/key>\s*<string>([^<]+)<\/string>/);
+
+                if (expirationDateMatch && expirationDateMatch[1] && nameMatch && nameMatch[1]) {
+                    const expirationDate = new Date(expirationDateMatch[1]);
+                    const name = nameMatch[1];
+
+                    const currentDate = new Date();
+                    if (expirationDate > currentDate) {
+                        return { name, expirationDate };
+                    }
+                }
+            } catch (error) {
+                console.error(`Error reading or parsing file ${filePath}: ${error.message}`);
+            }
+
+            return null;
+        }).filter(Boolean);
+
+        return profiles;
+    } catch (error) {
+        console.error(`Error reading provisioning profiles: ${error.message}`);
+        return [];
+    }
+}
+
+function getXcode16ProvisioningProfiles() {
+    const provisioningProfilesPath = path.join(process.env.HOME, 'Library', 'Developer', 'Xcode', 'UserData', 'Provisioning Profiles');
 
     try {
         const files = fs.readdirSync(provisioningProfilesPath);
